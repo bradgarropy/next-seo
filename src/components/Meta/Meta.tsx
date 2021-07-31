ac9d2a1d255c5729b24b1bb0b140a27fb9742999
Max Programming <ui.sabuwala@gmail.com>
2021-07-31 07:32:33 +0530

    ✨ Added additional meta and link tags

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,14 +1,25 @@
 import Head from "next/head"
 import {FC} from "react"
 
+import {LinkTag, MetaTag} from "../SEO/SEO"
+
 type MetaProps = {
     title?: string
     description?: string
     keywords?: string[]
     icon?: string
+    additionalMetaTags?: Array<MetaTag>
+    additionalLinkTags?: Array<LinkTag>
 }
 
-const Meta: FC<MetaProps> = ({title, description, keywords, icon}) => {
+const Meta: FC<MetaProps> = ({
+    title,
+    description,
+    keywords,
+    icon,
+    additionalMetaTags,
+    additionalLinkTags,
+}) => {
     return (
         <Head>
             {title && <title key="title">{title}</title>}
@@ -29,6 +40,27 @@ const Meta: FC<MetaProps> = ({title, description, keywords, icon}) => {
                 />
             )}
 
+            {additionalMetaTags &&
+                additionalMetaTags.map(meta => (
+                    <meta
+                        key={meta.name}
+                        name={meta.name}
+                        content={meta.content}
+                        httpEquiv={meta.httpEquiv}
+                        charSet={meta.charSet}
+                    />
+                ))}
+            {additionalLinkTags &&
+                additionalLinkTags.map(link => (
+                    <link
+                        key={link.type}
+                        rel={link.rel}
+                        href={link.href}
+                        type={link.type}
+                        sizes={link.sizes}
+                        color={link.color}
+                    />
+                ))}
             {icon && <link key="icon" rel="icon" href={icon} />}
         </Head>
     )
